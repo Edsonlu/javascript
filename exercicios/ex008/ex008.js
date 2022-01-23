5732aa3d6e4f711c9ec3e7e544e521e3af3f60ac
EDSON LUIZ DA SILVA <96879894+Edsonlu@users.noreply.github.com>
2022-01-23 19:32:30 -0300

    Finalizei exercício 9 proposto

---
+++ exercicios/ex008/ex008.js
@@ -0,0 +1,6 @@
+var vel = 100
+console.log(`A velocidade do seu carro é de ${vel} km/h`)
+if (vel > 60) {
+    console.log(`Você ultrapassou a velocidade permitida que é de 60km/h. MULTADO!!!`)
+}
+    console.log(`Dirija sempre usando o cinto de segurança!!!`)
