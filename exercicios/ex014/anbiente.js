1201723e8e80c490a0a91ef40e3a68425593b0b5
EDSON LUIZ DA SILVA <96879894+Edsonlu@users.noreply.github.com>
2022-01-24 19:14:35 -0300

    Novos exercícios de JS

---
+++ exercicios/ex014/anbiente.js
@@ -0,0 +1,10 @@
+/*
+var c = 1
+while (c <= 5) {
+    console.log(c)
+    c++
+}*/
+for (var c = 0;c <= 10; c++){
+    console.log(c)
+    c++
+}
---
+++ exercicios/ex016/fun.js
@@ -0,0 +1,8 @@
+function parimoar(n) {
+    if (n % 2 == 0) {
+        return `par!`
+    }else {
+        return `impar!`
+    }
+}
+console.log(parimoar(223))
