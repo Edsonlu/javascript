80fd93cf8bbdb65b9842356f2ec665dd0c4b897b
EDSON LUIZ DA SILVA <96879894+Edsonlu@users.noreply.github.com>
2022-06-25 13:38:58 -0300

    Novas aulas

---
+++ javascrit-cfb cursos/Aula 057/aula57.js
@@ -0,0 +1,35 @@
+//Elementos
+var vbtIniciar
+var vbola
+var vcpu
+var vjogador
+var vPaineltxtPontos
+
+//Controles de Animação
+var game,franes
+
+//Posições
+var posBolaX, posBolaY
+var posJogadorX, posJogadorY
+var posCpuX, posCpuY
+
+//Direção de acordo com a tecla
+var dirJy
+
+//Posições iniciais
+var posJogIniY=180,posCpuIniY=180,posBolaIniX=475,posBolaIniY=240
+
+//Tamanhos
+var campoX=0,campoY=0,campoW=960,campoH=500
+var barraW=20,barraH=140,bolaW=20,bolaH=20
+
+//Direção
+var bolaX,bolaY 
+var jogY=0,cpuY=0
+
+//Velocidade
+var velBola,velCpu,velJogador
+
+//Controle
+var pontos=0
+var tecla
