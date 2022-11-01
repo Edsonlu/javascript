dd9439860d6ebc0ada8f7be33204b574c1b010b4
EDSON LUIZ DA SILVA <96879894+Edsonlu@users.noreply.github.com>
2022-11-01 18:49:42 -0300

    Novas aulas

---
+++ javascrit-cfb cursos/Aula 137/jogador.js
@@ -0,0 +1,36 @@
+class jogador{
+  constructor(ctx,teclado){
+    this.ctx=ctx
+    this.teclado=teclado
+    this.x=0
+    this.y=0
+    this.vel=3
+    this.nave= new Image()
+    this.nave.src="nave.png"
+    this.nave.addEventListener('load',()=>{
+      this.desenhar()
+     })
+  }
+  gerenciar(){
+    if(this.teclado.esquerda){
+      if (this.x > 0)
+        this.x-=this.vel
+    }
+    if(this.teclado.direita){
+      if(this.x < this.ctx.canvas.width-this.nave.width)
+      this.x+=this.vel
+    }
+    if(this.teclado.cima){
+      if(this.y > 0)
+      this.y-=this.vel
+    }
+    if(this.teclado.baixo){
+      if(this.y < this.ctx.canvas.height-this.nave.height)
+      this.y+=this.vel
+    }
+  }
+  desenhar(){
+    this.gerenciar()
+    this.ctx.drawImage(this.nave,this.x,this.y)
+  }
+}
